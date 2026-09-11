64956dac7e4a076986b526642c5e2156655cb36e
Ben Croker <ben@putyourlightson.com>
2026-09-11 12:52:50 +0200

    Remove `pluginUntilBuild`

--- build.gradle.kts
+++ build.gradle.kts
@@ -60,7 +60,7 @@ intellijPlatform {
 
         ideaVersion {
             sinceBuild = providers.gradleProperty("pluginSinceBuild")
-            untilBuild = providers.gradleProperty("pluginUntilBuild")
+            untilBuild.convention(null as String?)
         }
     }
 
